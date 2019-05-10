f6008121338858c5a2bdd2856b352a3c92432e70
billbunkum <billbunkum@billbunkums-MacBook-Air.local>
2019-05-10 18:15:47 -0400

    add low & mid-level error handling

--- weather-app/app.js
+++ weather-app/app.js
@@ -7,8 +7,9 @@ const url = `https://api.darksky.net/forecast/${darkskyAPIKey}/37.8267,-122.4233
 
 // MAPBOX KEY
 const mapboxKEY = `pk.eyJ1IjoiYmlsbGJ1bmt1bSIsImEiOiJjanZjeGt5cXAxZmNzNDNyeGZpOGVucTltIn0.7UCD_eQUDmoI1fRJnWrGJw`;
+let location = '';
 // DARK SKY KEY
-const mapboxURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/reno.json?access_token=${mapboxKEY}&limit=1`;
+const mapboxURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${location}.json?access_token=${mapboxKEY}&limit=1`;
 // request( { url: url, json: true }, (error, response) => {
 // 	// USE IF 'json: true' OPTION IS NOT USED
 // 	// const data = JSON.parse(response.body);
@@ -16,15 +17,23 @@ const mapboxURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/reno.json?a
 // 	console.log(response.body.currently);
 // });
 
-request( { url: url, json: true }, (error, response) => {
-	let precipProb = response.body.currently.precipProbability;
-	let apparentTemp = response.body.currently.apparentTemperature;
-	console.log(response.body.daily.data[0].summary + ` It is currently ${apparentTemp} degrees out. There is a ${precipProb}% chance of rain.`);
-});
+// request( { url: url, json: true }, (error, response) => {
+// 	let precipProb = response.body.currently.precipProbability;
+// 	let apparentTemp = response.body.currently.apparentTemperature;
+// 	console.log(response.body.daily.data[0].summary + ` It is currently ${apparentTemp} degrees out. There is a ${precipProb}% chance of rain.`);
+// });
 
 request( { url: mapboxURL, json: true }, (error, response) => {
-	// console.log('response =>:', response);
-	let latitude = response.body.features[0].geometry.coordinates[1]
-	let longitude = response.body.features[0].geometry.coordinates[0];
-	console.log(`Longitude is ${longitude} and Latitude is ${latitude}`);
+	// LOW LEVEL 'error' HANDLING
+	if( error) {
+		console.log('error:', `${error}`);
+	} else if( response.body.message) {
+		console.log(`error: Location ${response.body.message}`);
+	} else {
+		// console.log('response =>:', response);
+		let latitude = response.body.features[0].geometry.coordinates[1]
+		let longitude = response.body.features[0].geometry.coordinates[0];
+		console.log(`Longitude is ${longitude} and Latitude is ${latitude}`);
+	}
 });
+
