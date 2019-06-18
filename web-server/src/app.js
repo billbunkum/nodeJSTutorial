f8dd965fc286ce22208522b13653729d97e6d751
billbunkum <billbunkum@billbunkums-MacBook-Air.local>
2019-06-18 14:30:26 -0400

    render 404

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,6 +2,7 @@
 const path = require('path');
 // FOR ROUTING-EASE
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
@@ -15,12 +16,15 @@ console.log(path.join(__dirname, '../public'));
 // CUSTOMIZE SERVER W/ app.use()
 	// ALLOWS express TO CHECK THIS PATH FOR path names AND SO DO NOT NEED app.get('pathName') AS BELOW FOR SERVING UP html docs
 const publicDirectoryPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // SETUP hbs ENGINE AND views/templates LOCATION
 app.set('view engine', 'hbs');
+
 // BY DEFAULT, templates/ IS CALLED views/, WE CHANGE IT TO views/ HERE JUST TO SHOW HOW IT CAN BE CHANGED IN hbs
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 app.use(express.static(publicDirectoryPath));
 
@@ -41,24 +45,10 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
 	res.render('help', {
-		title: 'help page',
+		title: 'Help page',
+		name: 'iiixi',
 		helpText: "here's some text"
 	});
-});
-
-// ROUTING BY HAND
-	// DO NOT NEED COS app.use SERVES UP HOME PATH
-	// app.get('', (req, res) => {
-	// 	res.send('Hello Express!');
-	// });
-app.get('/testPage1', (req, res) => {
-	res.send({
-		name: 'Bill',
-		age: 37
-	});
-});
-app.get('/testPage2', (req, res) => {
-	res.send('<title>Test Page2 Here</title>');
 });
 
 // ROUTING W/ OBJECTS, ARRAYS, JSON, etc.
@@ -68,6 +58,30 @@ app.get('/weather', (req, res) => {
 		location: 'KY'
 	});
 });
+
+// CATCH 404's
+app.get('*', (req, res) => {
+	res.render('404', {
+		title: '404 Page',
+		name: 'iiixi'
+	});
+});
+
+// ROUTING BY HAND
+	// DO NOT NEED COS app.use SERVES UP HOME PATH
+	// app.get('', (req, res) => {
+	// 	res.send('Hello Express!');
+	// });
+// app.get('/testPage1', (req, res) => {
+// 	res.send({
+// 		name: 'Bill',
+// 		age: 37
+// 	});
+// });
+// app.get('/testPage2', (req, res) => {
+// 	res.send('<title>Test Page2 Here</title>');
+// });
+
 
 // LISTENER
 app.listen(3000, () => {
