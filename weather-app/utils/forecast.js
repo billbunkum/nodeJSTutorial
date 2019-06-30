af6d81fc947a4a23614124790d2197b742b61a38
billbunkum <billbunkum@billbunkums-MacBook-Air.local>
2019-06-30 12:35:51 -0400

    finish front-end using forecast

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -5,18 +5,18 @@ const request = require('request');
 const forecast = (latitude, longitude, callback) => {
 	const darkskyAPIKey = '297842be681beeba3e18b115c8bab523';
 		// const units = '&limit=1';
-		const darkskyURL = `https://api.darksky.net/forecast/${darkskyAPIKey}/` + `${latitude},${longitude}`;
+	const darkskyURL = `https://api.darksky.net/forecast/${darkskyAPIKey}/` + `${latitude},${longitude}`;
 		 // + `${units}`;
-		request( {url: darkskyURL, json: true}, (error, { body }) => {
-			// RETURNS response.body.code...
-			if(error) {
-				callback('Cannot connect', undefined);
-			} else if(body.code === 400) {
-				callback('Location invalid', undefined);
-			} else {
-				callback(undefined, body);
-			}
-		});
+	request( {url: darkskyURL, json: true}, (error, { body }) => {
+		// RETURNS response.body.code...
+		if(error) {
+			callback('Cannot connect', undefined);
+		} else if(body.code === 400) {
+			callback('Location invalid', undefined);
+		} else {
+			callback(undefined, body);
+		}
+	});
 };
 
 module.exports = forecast;
